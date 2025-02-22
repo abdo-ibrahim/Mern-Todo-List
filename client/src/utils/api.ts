972355d6a6eb47ca0baab14542e8972fef2bae01
Abdulrahman Ibrahim <43120443+abdo-ibrahim@users.noreply.github.com>
2025-02-23 00:43:59 +0200

    Update api.ts

--- client/src/utils/api.ts
+++ client/src/utils/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 import getToken from "./getToken";
-export const baseURL = "http://localhost:5000/api/v1";
+export const baseURL = "mern-todo-list-onis-backend.vercel.app/api/v1";
 
 export const todoApi = `${baseURL}/todos`;
 export const authApi = `${baseURL}/auth`;
