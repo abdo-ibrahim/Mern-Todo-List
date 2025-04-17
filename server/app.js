d1fe823ba4af12b1ce19e2b427f63aee64c297a5
Abdo_Ibrahim <abdo.ibrahim411@gmail.com>
2025-04-17 18:24:22 +0200

    update cors

--- server/app.js
+++ server/app.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(
   cors({
     // origin: ["http://localhost:5173"],
-    origin: ["https://mern-todo-list-xi-five.vercel.app"],
+    origin: "https://mern-todo-list-xi-five.vercel.app",
     credentials: true,
   })
 );
